893128d1b1ffcdf8423e982d41979934da7be54c
mnielsen-dk <mn@telosteam.dk>
2019-12-12 23:12:56 +0100

    latest source

---
+++ TTTWordHelper/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace TTTWordHelper
+{
+    public class Class1
+    {
+        public Microsoft.Office.Interop.Word.Application app;
+
+        public test()
+        {
+            app = app.ApplicationClass();
+            app.Documents.Open()
+        }
+    }
+}
